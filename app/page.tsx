34f6336668ba7bee4b485864787ddd45b606575b
Bluemystridge <alvaro9ridge@gmail.com>
2026-09-03 13:51:07 +0200

    Create home page

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import { Hero } from '@/components/Hero'
+import { Services } from '@/components/Services'
+import { Testimonials } from '@/components/Testimonials'
+import { CallToAction } from '@/components/CallToAction'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <Testimonials />
+      <CallToAction />
+    </>
+  )
+}
